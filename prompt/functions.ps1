6bda743e92ed329e2b191236c23c577b6c7bc4d1
Dan Albrecht <Dan-Albrecht@users.noreply.github.com>
2021-02-27 01:57:08 -0800

    Basic prompt

--- prompt/functions.ps1
+++ prompt/functions.ps1
@@ -9,3 +9,15 @@ Function CreateDynamicAlias() {
     Invoke-Expression $dynamicFunction
     Set-Alias -Name $name -Value $functionName -Scope Global
 }
+
+function Write-TerminatingError {
+    param (
+        [Parameter(Mandatory = $true)][string]$message
+    )
+
+    # Don't want all the crap powershell would normally print out
+    # $ErrorView = 'ConciseView' doesn't apply to scripts so
+    # create a simple good enough one
+    Write-Host -ForegroundColor Red -Object $message
+    exit
+}
--- prompt/prompt.ps1
+++ prompt/prompt.ps1
@@ -17,14 +17,18 @@ param (
     [Parameter(Mandatory = $true)][string]$repoName,
     [Parameter(Mandatory = $false)][int]$relaunchMeExitCode = 0)
 
-. $PSScriptRoot\functions.ps1
-
+$ErrorActionPreference = "Stop"
+$ohMyVersion = "3.98.0"
 $vsWherePath = "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe"
+
+. $PSScriptRoot\functions.ps1
 
 if (Test-Path -Path $vsWherePath) {
     $vsInstallPath = & $vsWherePath -prerelease -latest -property installationPath
+    $vsToolsDisplayName = & $vsWherePath -prerelease -latest -property catalog_productDisplayVersion
     Import-Module (Get-ChildItem $vsInstallPath -Recurse -File -Filter Microsoft.VisualStudio.DevShell.dll).FullName
-    Enter-VsDevShell -VsInstallPath $vsInstallPath -DevCmdArguments '-arch=x64'
+    Enter-VsDevShell -VsInstallPath $vsInstallPath -DevCmdArguments '-arch=x64' | Out-Null
+    Write-Host "Tools version: $vsToolsDisplayName"
 }
 else {
     Write-Warning "VS was not found so you're not gonna have (m)any build tools"
@@ -40,6 +44,21 @@ Remove-Alias -Name gp -Force -ErrorAction Ignore
 CreateDynamicAlias -name "nt" -action "Set-Location -Path '$repoPath'"
 CreateDynamicAlias -name "gs" -action "git status"
 CreateDynamicAlias -name "gf" -action "git fetch origin main"
+CreateDynamicAlias -name "re" -action "exit $relaunchMeExitCode"
 
 Set-Location -Path $repoPath
 $Host.UI.RawUI.WindowTitle = $repoName
+
+$omp = Get-Module -Name oh-my-posh -ListAvailable
+
+if ($null -eq $omp) {
+    Write-TerminatingError "Missing prompt, run: Install-Module -Name oh-my-posh -RequiredVersion $ohMyVersion"
+}
+
+if ($null -eq ($omp.Version | Where-Object { $_ -eq $ohMyVersion })) {
+    $foundVersions = $omp.Version
+    Write-TerminatingError "Expect to find version $ohMyVersion, but found $foundVersions. Update prompt settings or install via: Install-Module -Name oh-my-posh -RequiredVersion $ohMyVersion"
+}
+
+Import-Module -Name oh-my-posh -RequiredVersion $ohMyVersion
+Set-PoshPrompt -Theme $PSScriptRoot\ohMyPosh.json
