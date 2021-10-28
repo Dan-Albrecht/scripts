3ebb320ee898b34511c8a37626d9202bcd4bac5f
Dan Albrecht <Dan-Albrecht@users.noreply.github.com>
2021-10-27 22:49:19 -0700

    Stop escaping, terminal has a normal editor now

--- prompt/generatePrompt.ps1
+++ prompt/generatePrompt.ps1
@@ -5,7 +5,6 @@ Prints a command line to start everything.
 $runningExe = (Get-Process -Id $PID).Path
 $repoName = (New-Object System.IO.DirectoryInfo($PWD)).Name
 $result = "$runningExe -Interactive -NoLogo -Command $PSScriptRoot\startPrompt.ps1 -repoPath $PWD -repoName $repoName"
-$result = $result.Replace('\', '\\')
 
 Write-Host "Basic version:"
 Write-Host $result
