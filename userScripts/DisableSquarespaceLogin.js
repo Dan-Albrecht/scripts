56b932e3da235c11b8165f1fc1119dd26c630bdc
Dan Albrecht <Dan-Albrecht@users.noreply.github.com>
2022-12-02 21:40:53 -0800

    v1; see if update works; get rid of debug messages

--- userScripts/DisableSquarespaceLogin.js
+++ userScripts/DisableSquarespaceLogin.js
@@ -3,18 +3,13 @@
 // @namespace   https://github.com/Dan-Albrecht/miscellaneous
 // @match       *://*/*
 // @grant       none
-// @version     0.9
+// @version     1.0
 // @author      Dan Albrecht
-// @description 12/2/2022, 8:50:35 PM
+// @description Disables the horribly annoying behavior of Squarespace sites redirecting to the login page on the press of the escape key. 
 // ==/UserScript==
 
 if (typeof (Static) !== 'undefined') {
   if (Static?.SQUARESPACE_CONTEXT?.websiteSettings?.useEscapeKeyToLogin === true) {
     Static.SQUARESPACE_CONTEXT.websiteSettings.useEscapeKeyToLogin = false
-    console.log('disabled login')
-  } else {
-    console.log('login hook was not on')
   }
-} else {
-  console.log('not a square site')
 }
